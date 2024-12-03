2a7241adfa4821e3ea049f56bf77b716a280a565
feather2308 <feather2308@naver.com>
2024-12-03 15:12:08 +0900

    search 재료 자동완성 추가

---
+++ src/main/java/com/team9/alddalddal/controller/SearchRestController.java
@@ -0,0 +1,22 @@
+package com.team9.alddalddal.controller;
+
+import com.team9.alddalddal.service.CocktailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/getIngredients")
+public class SearchRestController {
+    @Autowired
+    private CocktailService cocktailService;
+
+    @GetMapping("/find")
+    public List<String> getIngredients(@RequestParam("query") String name) {
+        return cocktailService.findByIngredientsNameContaining(name);
+    }
+}
