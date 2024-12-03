af770e2f607fc2ab351bcefae723695b3c6fcbd7
feather2308 <feather2308@naver.com>
2024-12-03 14:41:47 +0900

    search 추가

--- src/main/java/com/team9/alddalddal/repository/Recipe_IngredientRepository.java
+++ src/main/java/com/team9/alddalddal/repository/Recipe_IngredientRepository.java
@@ -1,6 +1,7 @@
 package com.team9.alddalddal.repository;
 
 import com.team9.alddalddal.entity.Recipe_Ingredient;
+import com.team9.alddalddal.entity.Recipe_IngredientId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface Recipe_IngredientRepository extends JpaRepository<Recipe_Ingredient, Long> {
     List<Recipe_Ingredient> findById_Cname(String cocktail_name);
     List<Recipe_Ingredient> findById_Iname(String ingredient_name);
+    List<Recipe_Ingredient> findById(Recipe_IngredientId id);
 }
