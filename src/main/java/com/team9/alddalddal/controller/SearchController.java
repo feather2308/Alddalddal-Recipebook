e1015b0d0c99c966e8e912b2c700f43d31a8af5c
feather2308 <feather2308@naver.com>
2024-12-03 15:12:08 +0900

    search 재료 자동완성 추가

--- src/main/java/com/team9/alddalddal/controller/SearchController.java
+++ src/main/java/com/team9/alddalddal/controller/SearchController.java
@@ -1,6 +1,7 @@
 package com.team9.alddalddal.controller;
 
 import com.team9.alddalddal.entity.Cocktail;
+import com.team9.alddalddal.repository.Recipe_IngredientRepository;
 import com.team9.alddalddal.service.CocktailService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
