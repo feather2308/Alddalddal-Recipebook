c95168a532024ebc3b3f2a454b23399cdbd13864
feather2308 <feather2308@naver.com>
2024-12-03 14:41:47 +0900

    search 추가

--- src/main/java/com/team9/alddalddal/repository/RecipeRepository.java
+++ src/main/java/com/team9/alddalddal/repository/RecipeRepository.java
@@ -3,6 +3,8 @@ package com.team9.alddalddal.repository;
 import com.team9.alddalddal.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-    Recipe findByName(String name);
+    Optional<Recipe> findByName(String name);
 }
